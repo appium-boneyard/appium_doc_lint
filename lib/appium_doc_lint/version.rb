9a9eaa9854c2f16409d06333b120937995a42af0
bootstraponline <code@bootstraponline.com>
2014-04-26 17:24:57 -0400

    Release 0.0.7

--- lib/appium_doc_lint/version.rb
+++ lib/appium_doc_lint/version.rb
@@ -1,6 +1,6 @@
 module Appium
   class Lint
-    VERSION = '0.0.6' unless defined? ::Appium::Lint::VERSION
+    VERSION = '0.0.7' unless defined? ::Appium::Lint::VERSION
     DATE    = '2014-04-26' unless defined? ::Appium::Lint::DATE
   end
 end
